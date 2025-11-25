b6bea1354e0bd95507c94d03d9a34867f6dd8a30
paulanavarro01 <132442184+paulanavarro01@users.noreply.github.com>
2025-11-25 12:42:37 +0100

    algorithms options added

--- myapp/search/search_engine.py
+++ myapp/search/search_engine.py
@@ -1,63 +1,26 @@
 import random
 import numpy as np
 
-from myapp.search.objects import Document
-from project_progress.part_2.indexing_evaluation import (
-    load_processed_docs,
-    create_index_tfidf,
-    search_tf_idf
-)
-
-docs = load_processed_docs()                          # List of dicts
-index, tf, df, idf, title_index = create_index_tfidf(docs)
-
-# Build a PID → full document lookup (dictionary)
-full_doc_lookup = {doc["pid"]: doc for doc in docs}
-
-
-def tfidf_search(corpus: dict, search_query, search_id, num_results=20):
-    """
-    Executes your TF-IDF search and returns Document objects for the UI.
-    """
-
-    ranked_pids = search_tf_idf(search_query, index, tf, idf, title_index)
-
-    results = []
-
-    for pid in ranked_pids[:num_results]:
-
-        raw = corpus.get(pid)                    # This is a Document object
-        proc = full_doc_lookup.get(pid, {})      # This is the dict from processed docs
-
-        if raw is None:
-            continue
-
-        title = raw.title or proc.get("title_clean") or "[No Title]"
-        description = raw.description or proc.get("description_clean") or ""
-
-        results.append(
-            Document(
-                pid=pid,
-                title=title,
-                description=description,
-                url=f"doc_details?pid={pid}&search_id={search_id}",
-                ranking=random.random()
-            )
-        )
-
-    return results
+from myapp.search.algorithms import (tfidf_search, bm25_search, price_boost_search)
 
 
 
 class SearchEngine:
     """Class that implements the search engine logic"""
 
-    def search(self, search_query, search_id, corpus):
+    def search(self, search_query, search_id, corpus, method="bm25"):
         print("Search query:", search_query)
-
         results = []
-        ### You should implement your search logic here:
-        results = tfidf_search(corpus, search_query, search_id)  # replace with call to search algorithm
 
-        # results = search_in_corpus(search_query)
+        # choose algorithm based on method
+        if method == "bm25":
+            from myapp.search.algorithms import bm25_search
+            results = bm25_search(corpus, search_query, search_id)
+        elif method == "price_boost":
+            from myapp.search.algorithms import price_boost_search
+            results = price_boost_search(corpus, search_query, search_id)
+        else:
+            from myapp.search.algorithms import tfidf_search
+            results = tfidf_search(corpus, search_query, search_id)
+
         return results
